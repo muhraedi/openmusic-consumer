7a2de572744d6d706b2ec09906d3c447b7d36222
muhraedi <muhraedi@gmail.com>
2023-05-17 16:45:32 +0800

    fix content exports

--- src/PlaylistSongsService.js
+++ src/PlaylistSongsService.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+const mapDBToModelPlaylist = require('./utils');
 
 class PlaylistSongsService {
   constructor() {
     this._pool = new Pool();
   }
 
-  async getPlaylistSongById(id) {
+  async getPlaylistSongById(playlistId) {
     const query = {
       text: `SELECT playlist_songs.*, songs.title, songs.performer, playlists.*, users.username
       FROM playlist_songs
@@ -13,10 +14,12 @@ class PlaylistSongsService {
       LEFT JOIN playlists ON playlists.id = playlist_songs.playlist_id
       LEFT JOIN users on users.id = playlists.owner
       WHERE playlist_songs.playlist_id = $1`,
-      values: [id],
+      values: [playlistId],
     };
+    console.log(playlistId);
     const result = await this._pool.query(query);
-    return result.rows;
+    const playlist = mapDBToModelPlaylist(result.rows);
+    return playlist;
   }
 }
 
---
+++ src/utils.js
@@ -0,0 +1,22 @@
+const mapDBToModelPlaylist = (dataFromDB) => {
+  const playlist = {
+    id: dataFromDB[0].id,
+    name: dataFromDB[0].name,
+    username: dataFromDB[0].username,
+    songs: [],
+  };
+
+  dataFromDB.forEach((el) => {
+    const song = {
+      id: el.song_id,
+      title: el.title,
+      performer: el.performer,
+    };
+
+    playlist.songs.push(song);
+  });
+
+  return playlist;
+};
+
+module.exports = mapDBToModelPlaylist;
